e5c235f4d93aadc433d4f1a22a92479fa6e1b1d7
Ilya Schutskiy <ilyaschutskiy@MBP-admin.Dlink>
2019-11-17 15:55:15 +0300

    route

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
+import { Route, Switch } from 'react-router-dom';
 import React, { Component } from 'react'
 import { Menu, NewColor, Colors } from './containers'
 import '../stylesheets/APP.scss'
 
 const App = () =>
-    <div className="app">
-        <Menu />
-        <NewColor />
-        <Colors />
-    </div>
+    <Switch>
+        <Route exact path="/:id"  component={Colors} />
+        <Route path="/" component={() => (
+            <div className="app">
+                <Menu />
+                <NewColor />
+                <Colors />
+            </div>
+        )} />
+    </Switch>
 
 export default App
--- src/index.js
+++ src/index.js
@@ -11,27 +11,13 @@ const store = storeFactory();
 window.React = React;
 window.store = store;
 
-/*
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)*/
 
 render(
-    <HashRouter>
-        <div className="main">
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/events" component={Events} />
-                <Route path="/products" component={Products} />
-                <Route path="/contact" component={Contact} />
-                <Route component={Whoops404} />
-            </Switch>
-        </div>
-    </HashRouter>,
-    document.getElementById('root')
+  <Provider store={store}>
+      <HashRouter>
+          <App />
+      </HashRouter>
+  </Provider>,
+  document.getElementById('root')
 )
 
